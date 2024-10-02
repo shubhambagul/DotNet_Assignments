0d072cf5a0f702845e3a339b9912a0a6c17a9615
Shubham <Shubham@LAPTOP-GD1DB339>
2024-10-02 12:36:10 +0530

    Assignment of ArrayList and Hashtable

---
+++ DotNet_Assignments/ArrayListEx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_Assignments
+{
+    internal class ArrayListEx
+    {
+        static void Main()
+        {
+            ArrayList al = new ArrayList();
+            al.Add(10);
+            al.Add(20);
+            al.Add("Hello");
+            al.Add(10.5);
+            al.Add('S');
+            al.Add(30);
+            al.Add(9764904458);
+            al.Add(40);
+            al.Add(false);
+            al.Add(50);
+            Console.WriteLine("Total Elements of ArrayList");
+            foreach (var items in al)
+            {
+                Console.WriteLine(items);
+            }
+            Console.WriteLine("Removed the Elements Within ArrayList ");
+
+            al.Remove(false);
+            al.RemoveAt(3);
+            al.Reverse();
+            foreach (var items in al)
+            {
+                Console.WriteLine(items);
+            }
+        }
+    }
+}
---
+++ DotNet_Assignments/HashTableEx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_Assignments
+{
+    internal class HashTableEx
+    {
+        static void Main()
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add(101, "Hello");
+            ht.Add(102, 10.5);
+            ht.Add(103, 'S');
+            ht.Add(104, false);
+            ht.Add(105, 1000);
+            Console.WriteLine("Before Removing Elements Of Hashtable ");
+            foreach (DictionaryEntry entry in ht)
+            {
+                Console.WriteLine($"Key : {entry.Key}, Value : {entry.Value}");
+            }
+            ht.Remove(102);
+            ht.Remove(104);
+            Console.WriteLine("After Removing Elements Of Hashtable ");
+            foreach (DictionaryEntry entry in ht)
+            {
+                Console.WriteLine($"Key : {entry.Key}, Value : {entry.Value}");
+            }
+
+        }
+    }
+}
