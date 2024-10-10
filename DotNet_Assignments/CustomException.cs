ed2c446291b7ace7d47f455d6ff0afa8c63abf98
Shubham <Shubham@LAPTOP-GD1DB339>
2024-10-10 11:37:54 +0530

    Assignment of Exception Handling

---
+++ DotNet_Assignments/CustomException.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_Assignments
+{
+    internal class NegativeNumberException : Exception
+    {
+        public NegativeNumberException(string? message) : base(message)
+        {
+        }
+    }
+    internal class CustomException
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Enter possitive number");
+                int num = Convert.ToInt32(Console.ReadLine());
+                if (num < 0)
+                {
+                    throw new NegativeNumberException("Negative number are not allowed");
+                }
+                else
+                {
+                    Console.WriteLine(num);
+                }
+            }
+            catch (NegativeNumberException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+
+
+}
---
+++ DotNet_Assignments/ExcetionHandaling_ArithmaticOperation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_Assignments
+{
+    internal class ExcetionHandaling_ArithmaticOperation
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.Write("Enter the first number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter the Last number: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"Addition of Two Digit = {num1 + num2}");
+                Console.WriteLine($"Substraction of Two Digit = {num1 - num2}");
+                Console.WriteLine($"Multiplication of Two Digit = {num1 + num2}");
+                Console.WriteLine($"Divided of Two Digit = {num1 / num2}");
+
+
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArithmeticException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
