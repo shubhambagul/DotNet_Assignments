2dd99d88c7a8e5f3dae6a5084d919ce510c19531
Shubham <Shubham@LAPTOP-GD1DB339>
2024-10-11 15:39:41 +0530

    Assignment of Single cast and Multicast Delegate

---
+++ Delegates1/MulticastDelegates.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates1
+{
+    public delegate void Notify(int a, int b);
+
+    class Delegates1Impl
+    {
+        public void Addition(int a, int b)
+        {
+            Console.WriteLine("Addition = " + (a + b));
+        }
+        public void Substraction(int a, int b)
+        {
+            Console.WriteLine("Substraction = " + (a - b));
+        }
+        public void Multiplication(int a, int b)
+        {
+            Console.WriteLine("Multiplication = " + (a * b));
+        }
+    }
+    internal class MulticastDelegates
+    {
+        static void Main()
+        {
+            Delegates1Impl dI = new Delegates1Impl();
+            Notify notify = dI.Addition;
+
+            notify += dI.Substraction;
+
+            notify += dI.Multiplication;
+
+            notify(20, 5);
+        }
+    }
+}
---
+++ Delegates1/SingleCastDelegates.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates1
+{
+    public delegate void AdditionDelegate(int a, int b);
+    public delegate int SubstractionDelegate(int x, int y);
+
+    class DelegatesImpl
+    {
+        public void Addition(int a, int b)
+        {
+            Console.WriteLine("Addition of Two Number is " + (a + b));
+        }
+        public int Substraction(int x, int y)
+        {
+            return x - y;
+        }
+    }
+    internal class SingleCastDelegates
+    {
+        static void Main()
+        {
+            DelegatesImpl obj = new DelegatesImpl();
+           
+            AdditionDelegate ad = new AdditionDelegate(obj.Addition);
+            ad(30, 20);
+
+            SubstractionDelegate sd = new SubstractionDelegate(obj.Substraction);
+            int subst = sd(40, 10);
+            Console.WriteLine("Subtraction of Two Numbers is " + subst);
+        }
+    }
+
+}
