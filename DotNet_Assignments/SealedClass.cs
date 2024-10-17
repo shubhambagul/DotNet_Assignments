2436089ef9b2a5f835dc67df189aaabbc78ff277
Shubham <Shubham@LAPTOP-GD1DB339>
2024-10-17 09:47:40 +0530

    Assignment of sealed and static class

---
+++ DotNet_Assignments/SealedClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_Assignments
+{
+    class CalculateBaseClass
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+    sealed class CalculateDerivedClass : CalculateBaseClass
+    {
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+    }
+    internal class SealedClass
+    {
+        public static void Main()
+        {
+            CalculateDerivedClass obj = new CalculateDerivedClass();
+            int addition = obj.Add(10, 20);
+            Console.WriteLine(addition);
+            int substraction = obj.Add(40, 20);
+            Console.WriteLine(substraction);
+
+        }
+    }
+}
---
+++ DotNet_Assignments/StaticExample_CatWorld.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_Assignments
+{
+    static class Catworld
+    {
+        public static int NumberOfCats { get; private set; }
+        public static int TotalCatNaps { get; private set; }
+
+        static Catworld()
+        {
+            NumberOfCats = 10;
+        }
+        public static void AddCat()
+        {
+            NumberOfCats++;
+            Console.WriteLine($"New cat is joined, Available cats are {NumberOfCats}");
+        }
+        public static void AddCatNaps(int naps)
+        {
+            TotalCatNaps += naps;
+            Console.WriteLine($"Added {naps} cat naps, Total cat naps available now {TotalCatNaps}");
+        }
+
+    }
+    class StaticExample_CatWorld
+    {
+        public static void Main()
+        {
+            Catworld.AddCat();
+            Catworld.AddCat();
+            Catworld.AddCatNaps(10);
+            Catworld.AddCatNaps(5);
+
+        }
+    }
+}
