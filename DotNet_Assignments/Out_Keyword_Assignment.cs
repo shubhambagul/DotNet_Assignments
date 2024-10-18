41c8bbb3d69aa7d2a6ed6e461404ad26ff906883
Shubham <Shubham@LAPTOP-GD1DB339>
2024-10-18 14:23:54 +0530

    Assignment of Ref & Out Keywords

---
+++ DotNet_Assignments/Out_Keyword_Assignment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_Assignments
+{
+    internal class Out_Keyword_Assignment
+    {
+        public static void InitializeValues(out int number, out string name)
+        {
+            number = 10;
+            name = "hello";
+        }
+        public static void Main()
+        {
+            int value;
+            string text;
+            InitializeValues(out value, out text);
+            Console.WriteLine(text);
+            Console.WriteLine(value);
+        }
+    }
+}
---
+++ DotNet_Assignments/Ref_Assignment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_Assignments
+{
+    class Ref_Assignment
+    {
+        public static void IncreamentValue(ref int value) {
+            value++;
+        }
+        public static void Main() {
+            int num = 10;
+            Console.WriteLine($"Before increament value : {num}");
+            IncreamentValue(ref num);
+            Console.WriteLine($"After increament value : {num}");
+        }
+    }
+}
